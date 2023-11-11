61391158c7c5525dbd2959bf3a33f8b5919d2208
Tarun Nahak <tarunnahak25@gmail.com>
2023-11-11 20:39:05 +0530

    added faq page

--- main.go
+++ main.go
@@ -14,7 +14,17 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href="mailto:tarunnahak25@gmail.com">Tarun Nahak</a></p>`)
 }
 
-
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<b>Q: Is there a free version?</b><br/>
+	<p>A: Yes! we offer a free trial for 30 days on any paid plans.</p><br/>
+	
+	<b>Q: What are your support hours?</b><br/>
+	<p>We have support staff answering emails 24/7, through response times may be a bit slower on weekends.</p><br/>
+	
+	<b>Q: How do I contact support</b><br/>
+	<p>A: Email us <a emailto="support@lenslocked.com">support@lenslocked.com</a></p>`)
+}
 
 type Router struct{}
 
@@ -24,6 +34,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 		// OR
@@ -41,4 +53,3 @@ func main() {
 	fmt.Println("Starting the server on 3000...")
 	http.ListenAndServe(":3000", router)
 }
-
