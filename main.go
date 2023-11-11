75a9e35267fb21bc828070f4126b04dd5b72cc05
Tarun Nahak <tarunnahak25@gmail.com>
2023-11-11 21:31:39 +0530

    Added chi router

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,30 +28,12 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	<p>A: Email us <a emailto="support@lenslocked.com">support@lenslocked.com</a></p>`)
 }
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-		// OR
-		// w.WriteHeader(http.StatusNotFound)
-		// fmt.Fprintf(w, "Page not found")
-	}
-
-}
-
 func main() {
-	var router Router
-	// http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/contact", contactHandler)
-	// http.HandleFunc("/knowPath", pathHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) { http.Error(w, "Page not found", http.StatusNotFound) })
 	fmt.Println("Starting the server on 3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
