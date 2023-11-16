a180df6eda9171101251bac6a0b5ea8548661bee
Tarun Nahak <tarunnahak25@gmail.com>
2023-11-16 20:20:59 +0530

    Added the controller for static

---
+++ controllers/static.go
@@ -0,0 +1,13 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/nahaktarun/lenslocked/views"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
--- main.go
+++ main.go
@@ -2,46 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	controller "github.com/nahaktarun/lenslocked/controllers"
 	"github.com/nahaktarun/lenslocked/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	t, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template: %v", http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := "templates/home.gohtml"
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := "templates/contact.gohtml"
-	executeTemplate(w, tplPath)
-
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := "templates/faq.gohtml"
-	executeTemplate(w, tplPath)
-}
-
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controller.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+	r.Get("/contact", controller.StaticHandler(tpl))
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controller.StaticHandler(tpl))
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) { http.Error(w, "Page not found", http.StatusNotFound) })
 	fmt.Println("Starting the server on 3000...")
 	http.ListenAndServe(":3000", r)
